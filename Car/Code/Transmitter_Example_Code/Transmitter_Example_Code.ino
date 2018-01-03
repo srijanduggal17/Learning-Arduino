4fe7e623b75a69ddb7d57bc5244eb7ba585228d0
Srijan Duggal <srijanduggal17@gmail.com>
2018-01-03 09:01:22 -0600

    Reorganized folder and added design files

---
+++ Car/Code/Transmitter_Example_Code/Transmitter_Example_Code.ino
@@ -0,0 +1,32 @@
+
+int potPin = A5;
+//radio
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#define CE_PIN 9  
+#define CSN_PIN 10
+const uint64_t pipe = 0xE8E8F0F0E1LL; // Define the transmit pipe</p><p>/*-----( Declare objects )-----*/
+RF24 radio(CE_PIN, CSN_PIN); // Create a Radio
+long deg = 0;
+
+void setup() {
+  radio.begin();
+  radio.openWritingPipe(pipe);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int tf = analogRead(potPin);
+  deg = map(tf, 0, 1023, 0, 180);
+
+  //send signal to the quad
+  radio.write(&deg, sizeof(deg));
+
+  
+  Serial.print("Deg:\t");
+  Serial.println(deg);
+  
+}
+
+
